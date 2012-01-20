ec8274f1da8720235b6605d2166f9ec32b38fe57
Alexis Janvier <contact@alexisjanvier.net>
2012-01-20 16:48:50 +0100

    demo vinyle en css et jquery

--- index.php
+++ index.php
@@ -1,3 +1,5 @@
 <h1>PLAYER VINYLE HTML</h1>
 <p>Tutorial : <a href="tutos/The Wheels Of Steel_ An Ode To Turntables (in HTML).xhtml" target="_blank">local</a> - <a href="http://www.schillmania.com/" target="_blank">en ligne</a> </p>
 <p>Exemple : <a href="exemple/The Wheels Of Steel  Turntables in your browser (a web-based DJ prototype).html" target="_blank">local</a> - <a href="http://wheelsofsteel.net/" target="_blank">en ligne</a> </p>
+<br />
+<p><a href="exemple2/index.html">Simple en css</a></p>
